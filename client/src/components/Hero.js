5857e51e42a177a562414afcd75e6c57cec42504
Iwona Ozimek <az0r.alq@hotmail.com>
2022-06-21 02:50:59 +0200

    Updated main image

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -8,19 +8,19 @@ export default function Hero() {
         <div className="main--content mt-24 mb-24 flex flex-col sm:items-center lg:items-stretch">
           <div className="heading text-3xl sm:text-5xl md:text-6xl font-black leading-none">
             Buy books
-            <div className="text-purple-500">for kids.</div>
+            <div className="text-blue-500">for kids.</div>
           </div>
           <div className="description max-w-md my-5 text-lg leading-loose">
             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
             nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
             erat, sed diam voluptua.
           </div>
-          <Link to="/shop" className="text-center inline-block w-full sm:w-48 py-3 font-semibold tracking-wide rounded hocus:outline-none focus:shadow-outline transition duration-300 bg-purple-400 text-gray-100 hover:bg-purple-500">
+          <Link to="/shop" className="text-center inline-block w-full sm:w-48 py-3 font-semibold tracking-wide rounded hocus:outline-none focus:shadow-outline transition duration-300 bg-blue-400 text-gray-100 hover:bg-blue-500">
             Shop Now
           </Link>
         </div>
       </div>
-      <div className="right--column bg-purple-200 bg-cover bg-center ml-24 h-96 h-auto w-1/2 flex-1 bg-[url('https://images.unsplash.com/photo-1591951425328-48c1fe7179cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80')]"></div>
+      <div className="right--column bg-violet-200 bg-cover bg-center ml-24 h-96 h-auto w-1/2 flex-1 bg-[url('https://tinyurl.com/y54bjxn8')]"></div>
     </div>
   );
 }
