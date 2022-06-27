b42c796bcee051e7ed2b39f1b4547ca58d8be3d3
Rebecca Hogg <rebeccaeloisehogg@hotmail.com>
2022-06-27 19:31:32 +0400

    Shop latest

--- client/src/components/Catalogue.js
+++ client/src/components/Catalogue.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect} from "react";
 import { MdShoppingCart } from "react-icons/md";
 import { MdManageSearch } from "react-icons/md";
-import { Multiselect } from "multiselect-react-dropdown";
+import CategoryFilter from "./CategoryFilter";
 import '../App.css';
 
 export default function Catalogue(props) {
@@ -24,10 +24,10 @@ export default function Catalogue(props) {
 
                 <form>
                     <div class="mb-6 mr-6px flex justify-center">
-                        <div className="place-items-center">
+                        <div className="place-items-center pr-4">
                             <MdManageSearch size="2.5rem"/>
                         </div>
-                        <input type="search" value={search} class="bg-gray-50 ml-6px border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Type in a book title, author or genre" onChange={handleChange}/>
+                            <input type="search" value={search} class="bg-gray-50 ml-6px border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Type in a book title, author or genre" onChange={handleChange}/>
                     </div>                
                 </form>
 
@@ -56,7 +56,7 @@ export default function Catalogue(props) {
                             <p className="mt-1 text-sm text-gray-500">{book.author}</p>
                             <p className="text-sm font-medium text-gray-900">${book.price}</p>
                             </div>
-                            <button><MdShoppingCart /></button>
+                            <button className="pl-4"><MdShoppingCart /></button>
                         </div>
                     </div>
                 ))}
