ec85905accbcc4a2cf9c3c80b5f4edf426bcdb96
Rebecca Hogg <rebeccaeloisehogg@hotmail.com>
2022-06-29 23:31:19 +0400

    Shop UI upgraded, dropdown category filter added

--- client/src/components/Catalogue.js
+++ client/src/components/Catalogue.js
@@ -1,12 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { MdManageSearch } from "react-icons/md";
 import Product from "./Product";
 import '../App.css';
+import CategoryFilter from "./CategoryFilter";
 
-export default function Catalogue(props) {
+export default function Catalogue({books}) {
 
     const [search, setSearch] = useState("");
-    // const [bookGenre, setBookGenre] = useState("All");
+    const [bookGenre, setBookGenre] = useState("All");
 
     const handleChange = (event) => {
         let name = event.target.name;
@@ -16,15 +17,10 @@ export default function Catalogue(props) {
         console.log(search);
     };
 
-    // const handleChangeGenre = (value) => {
-    //     setBookGenreCB(value);
-    //     console.log(bookGenre);
-    // };
-
-    // const handleChangeCategory = (event) => {
-    //     let value = event.target.value;
-    //     setBookGenre(value);
-    // };
+    const handleChangeGenre = (value) => {
+        setBookGenre(value);
+        console.log(bookGenre);
+    };
 
     return (
 
@@ -41,15 +37,7 @@ export default function Catalogue(props) {
                             <input type="search" value={search} class="bg-gray-50 ml-10px border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Type in a book title, author or genre" onChange={handleChange}/>
                         </div>
                         <div className="w-1/3">
-                            <select 
-                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-                                onChange={(value) => handleChangeGenre(value)}>
-                                    <option selected>Search by genre</option>
-                                    <option value="Detective">Detective</option>
-                                    <option value="Animals">Animals</option>
-                                    <option value="Fantasy">Fantasy</option>
-                                    <option value="Picture Books">Picture Books</option>
-                            </select>
+                            <CategoryFilter setBookGenreCB={(value) => handleChangeGenre(value)}/>
                         </div>
                     </div>                
                 </form>        
@@ -57,9 +45,16 @@ export default function Catalogue(props) {
             </div>
 
             <div className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
-                {props.books.filter((book) => (book.title.toLowerCase().includes(search.toLowerCase()) ||
+                {books.filter((book) => (book.title.toLowerCase().includes(search.toLowerCase()) ||
                     book.genre.toLowerCase().includes(search.toLowerCase()) ||
                     book.author.toLowerCase().includes(search.toLowerCase())))
+                    .filter(book => {
+                        if (bookGenre === "All") { 
+                           return book 
+                         } else if (book.genre === bookGenre) {
+                           return book
+                         }
+                       })
                     .map((book) => (
                         <div key={book.id} className="group relative">
                             <Product book={book} />
--- client/src/components/Featured.js
+++ client/src/components/Featured.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Product from "../components/Product";
 
-export default function Featured(props) {
+export default function Featured({featuredBooks}) {
 
   return (
     <div className="container max-w-screen-xl mx-auto lg:py-24">
@@ -11,12 +11,12 @@ export default function Featured(props) {
 
       <div className="flex flex-col items-center lg:items-stretch lg:flex-row">
 
-        {props.featuredBooks.map((book) => (
+        {featuredBooks.map((book) => (
           <div className="cards mt-5 lg:w-1/5 xl:mx-4 rounded-md overflow-hidden"
             key={book.id}>
             <Product book={book} />
           </div>
-        ))}
+        ))};
         
       </div>
 
